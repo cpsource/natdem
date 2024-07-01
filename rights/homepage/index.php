aa0dce1ff2251670aa60ada813c109cc4a7f32f8
Cal Page <page.cal@gmail.com>
2024-07-01 04:20:43 -0400

    troubleshoot

--- rights/homepage/index.php
+++ rights/homepage/index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "<a> at rights/homepage/index.php</a><br>"
+
 get_story ( $cp_path . 'rights/book/Introduction.html' );
 <hr>
 get_story ( $cp_path . 'rights/homepage/Web_Project_Outline.html' );
