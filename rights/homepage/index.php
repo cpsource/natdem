9a7055b7fb668e8a7c51a75360014b09d112a9b3
Cal Page <page.cal@gmail.com>
2024-07-01 04:55:53 -0400

    debug

--- rights/homepage/index.php
+++ rights/homepage/index.php
@@ -2,8 +2,9 @@
 
 echo "<a> at rights/homepage/index.php</a><br>";
 
-get_story ( $cp_path . 'rights/book/Introduction.html' );
-<hr>
+/* get_story ( $cp_path . 'rights/book/Introduction.html' ); */
+/* <hr> */
+
 get_story ( $cp_path . 'rights/homepage/Web_Project_Outline.html' );
 
 ?>
