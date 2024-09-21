711ce757361138b07c27075a7965e3633924962d
Cal Page <page.cal@gmail.com>
2024-09-21 19:21:24 +0000

    support AbuseIPDB

--- footer.php
+++ footer.php
@@ -6,6 +6,14 @@
         <p>Copyright &copy; 2002 to 2024, William R. Page, Author</p>
         <a href="index.php" class="btn btn-primary">Home</a>
     </div>
+
+    <hr class="custom-hr" style="border: 2px solid lightgray; width: 50%;">
+    <div class="footer-text">
+    	 <a href="https://www.abuseipdb.com/user/166888" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
+	 <img src="https://www.abuseipdb.com/contributor/166888.svg" alt="AbuseIPDB Contributor Badge" style="width: 200px;">
+	 </a>
+    </div>
+
 </footer>
 
 <!-- </body> -->
