d30e76555f50791c0e2469b9471931346315fd4e
Cal Page <page.cal@gmail.com>
2024-09-21 19:26:35 +0000

    add some text to footer

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
 
     <hr class="custom-hr" style="border: 2px solid lightgray; width: 50%;">
     <div class="footer-text">
+        <p>We support these web sites.</p>
     	 <a href="https://www.abuseipdb.com/user/166888" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
 	<img src="https://www.abuseipdb.com/contributor/166888.svg" alt="AbuseIPDB Contributor Badge" style="width: 200px;box-shadow: 2px 2px 1px 1px rgba(0, 0, 0, .2);">
         </a>
